bb19ce4c45783a4fb6c3594f81710f207ede8cc7
James King <james.king@facepunchstudios.com>
2022-08-09 11:50:46 +0100

    Camera snap when far from target

--- code/MazingCamera.cs
+++ code/MazingCamera.cs
@@ -2,21 +2,29 @@
 
 namespace Mazing;
 
-public class MazingCamera: CameraMode
+public class MazingCamera : CameraMode
 {
 	public override void Update()
-	{
-		if (Local.Pawn is not AnimatedEntity pawn)
-			return;
+    {
+        Rotation = Rotation.FromYaw(90f) * Rotation.FromPitch(80f);
 
-		var center = pawn.Position.WithZ(0f) + Vector3.Up * 64;
+        if (Local.Pawn == null)
+        {
+            return;
+        }
 
-        Rotation = Rotation.FromYaw(90f) * Rotation.FromPitch( 80f );
-
-		var distance = 1600f * pawn.Scale;
+        var center = Local.Pawn.Position.WithZ(0f) + Vector3.Up * 64;
+        var distance = 1600f * Local.Pawn.Scale;
         var target = center - Rotation.Forward * distance;
 
-		Position = Vector3.Lerp( Position.WithZ( target.z ), target, 0.5f );
+        if ( (target - Position).LengthSquared > 128f + 128f )
+        {
+            Position = target;
+        }
+        else
+        {
+            Position = Vector3.Lerp(Position.WithZ(target.z), target, 0.5f);
+        }
 
 		FieldOfView = 20;
 
